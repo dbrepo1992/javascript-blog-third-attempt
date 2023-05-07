74af977a02cc8e3903b96a289fb79531df1e47c8
Damian Bugaj <damianbugajit@gmail.com>
2023-05-07 08:49:54 +0200

    added this to clickedElement function and used it to make links active after click

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 const titleClickHandler = function(event){
+  const clickedElement = this;
   console.log(event,'Link was clicked!');
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -9,9 +10,12 @@ const titleClickHandler = function(event){
     activeLink.classList.remove('active');
   }
 
-  /* add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
+  clickedElement.classList.add('active');
+  console.log('clickedElement:', clickedElement);
+
+  /* [DONE] remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll('.posts article.active');
 
