3f73d5adc279b3eed01d6a1256db1ff0b080720e
Damian Bugaj <damianbugajit@gmail.com>
2023-06-04 15:44:31 +0200

    generated links of tags needs to create cloud of tags in right column

--- js/script.js
+++ js/script.js
@@ -229,7 +229,7 @@ function generateTags(){
   /* [NEW] START LOOP: for each tag in allTags: */
   for(let tag in allTags){
   /* [NEW] generate code of a link and add it to allTagsHTML */
-    allTagsHTML += tag + ' (' + allTags[tag] + ') ';
+    allTagsHTML += '<li><a href="#tag-' + tag + '">' + ' (' + allTags[tag] + ') ' + tag + '</a></li>';  
   }
   /* [NEW] END LOOP: for each tag in allTags: */
 
